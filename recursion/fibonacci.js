4a599309e6f0b8844e9cd81b2e719de28357e279
fathyElgazar <gazzarDev@gmail.com>
2024-07-05 01:26:14 +0300

    another solution for fibonacci

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -3,4 +3,17 @@ function fib(num) {
   return fib(num - 1) + fib(num - 2);
 }
 
-console.log(fib(35));
+console.log(fib(0));
+
+// Solution for TOP
+function fibs(num) {
+  function innerFib(num, arr) {
+    if (num <= 0) return [];
+    if (num === 1) return [0];
+    if (num <= 2) return arr;
+    let nextFib = arr[arr.length - 1] + arr[arr.length - 2];
+    return innerFib(num - 1, [...arr, nextFib]);
+  }
+  return innerFib(num, [0, 1]);
+}
+console.log(fibs(0));
