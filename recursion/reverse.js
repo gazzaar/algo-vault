8b987c64f0905e7a79fea99cd7252c60269c8df4
fathyElgazar <gazzarDev@gmail.com>
2024-07-04 14:09:06 +0300

    another solution for reverse

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -3,3 +3,11 @@ function reverse(str) {
   return str.at(str.length - 1) + reverse(str.slice(0, -1));
 }
 console.log(reverse('gazzaar'));
+
+// Another solution
+function reverseString(str) {
+  if (str.length <= 1) return str;
+  return reverseString(str.slice(1)) + str[0];
+}
+
+console.log(reverseString('anotherString'));
